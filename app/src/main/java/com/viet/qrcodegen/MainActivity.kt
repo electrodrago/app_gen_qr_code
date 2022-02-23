d4a645f25c03a24b226a2b3b511ba9df7265f983
electrodrago <quocviet11102001@gmail.com>
2022-02-23 10:03:36 +0700

    More code

--- app/src/main/java/com/viet/qrcodegen/MainActivity.kt
+++ app/src/main/java/com/viet/qrcodegen/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.viet.qrcodegen
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
+import com.google.zxing.qrcode.encoder.QRCode
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +21,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ivQRcode = findViewById(R.id.ivQRCode)
+        etData = findViewById(R.id.etData)
+        btnGenerateQRcode = findViewById(R.id.btnGenerateQRCode)
+
+        btnGenerateQRcode.setOnClickListener {
+            val data = etData.text.toString().trim()
+
+            if (data.isEmpty()) {
+                Toast.makeText(this, "Enter some data", Toast.LENGTH_SHORT).show()
+            } else {
+                val writer = QRCodeWriter()
+                try {
+                    val bitMatrix = writer.encode(data, BarcodeFormat.QR_CODE, 512, 512)
+                    val width = bitMatrix.width
+                    val height = bitMatrix.height
+                    val bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+                    for (i in 0 until width) {
+
+                    }
+                } catch (e: WriterException) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
